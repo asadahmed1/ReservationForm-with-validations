c54dac5d660dfb576c32979044a913f7bec5d681
asadahmed1 <asadahmed6345@gmail.com>
2019-09-10 09:00:18 -0700

    Project loader

--- src/app/validations/validations.component.ts
+++ src/app/validations/validations.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { dateValidator } from '../Validators/dateVlaidation'
 import { emailValidator } from '../Validators/emailValidators'
+import {  NgxSpinnerService} from "ngx-spinner";
 // import {FormsModule, ReactiveFormsModule,AbstractControl} from '@angular/forms';
 @Component({
   selector: 'app-validations',
@@ -10,14 +11,16 @@ import { emailValidator } from '../Validators/emailValidators'
 })
 export class ValidationsComponent implements OnInit {
   public reservation = Array();
+
   //dropdown values of roomtype
+
   public rooms = ["Single Person", "Two Person", "Family"];
   public reservationForm: FormGroup;
-  constructor() {
-    this.reservationForm = this.createFormGroup();
-  }
 
-  ngOnInit() {
+  //here we used our ngx-spinner to initiate in our consturctor
+
+  constructor(private spinner: NgxSpinnerService) {
+    this.reservationForm = this.createFormGroup();
   }
 
   createFormGroup() {
@@ -27,7 +30,7 @@ export class ValidationsComponent implements OnInit {
       //email validation emailValidator defined in  validator folder
       email: new FormControl("", [Validators.required,emailValidator]),
       //Pickup validation
-     pickup: new FormControl("", [Validators.required]),
+      pickup: new FormControl("", [Validators.required]),
       //room type
       roomtype: new FormControl("", [Validators.required]),
       //arrival date dateValidator defined in validators folder
@@ -35,9 +38,9 @@ export class ValidationsComponent implements OnInit {
       //flight No
       flightno: new FormControl("", [Validators.required]),
       // Request validation
-     request: new FormControl("", [Validators.required]),
+      request: new FormControl("", [Validators.required]),
       //Guest No
-       guests: new FormControl("", [Validators.required])
+      guests: new FormControl("", [Validators.required])
     });
   }
 
@@ -107,5 +110,12 @@ revert() {
       this.reservationForm.get('request').setValue(this.reservation[index].request);
       this.reservationForm.get('guests').setValue(this.reservation[index].guests);
       this.reservationForm.get('arrivalDate').setValue(this.reservation[index].arrivalDate);
+  }
+  ngOnInit() {
+    // spinner time to show and hinde after 2 sec it will take us to our page
+    this.spinner.show();
+      setTimeout(() => {
+          this.spinner.hide();
+      }, 2000);
   }
 }
